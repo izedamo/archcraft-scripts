7952f6e5d82f2663c3ddffab6462e31780765ccf
adi1090x <adi1090x@gmail.com>
2021-05-23 22:58:07 +0530

    updated

--- change_style.sh
+++ change_style.sh
@@ -6,22 +6,29 @@
 dir="$HOME/.config/openbox/styles"
 
 # Lib
-if ! . "/usr/lib/archcraft/common/ac-include.cfg" 2>/dev/null; then
-    echo $"Error: Failed to source /usr/lib/archcraft/common/ac-include.cfg" >&2 ; exit 1
+if ! . "/usr/lib/archlabs/common/al-include.cfg" 2>/dev/null; then
+    echo $"Error: Failed to source /usr/lib/archlabs/common/al-include.cfg" >&2 ; exit 1
 fi
 
 # Styles
-styles=($(ls -p --hide="Default.sh" $HOME/.config/openbox/styles))
+styles=(`ls -p --hide={Default.sh,Beach-Bitmap.sh,Forest-Bitmap.sh} $dir`)
+bitmap=(`cd $dir && ls *-Bitmap.sh`)
 
 gen_menu () {
     local count=1
+    local bcount=1
 
 	menuStart
 	menuItem "Default" "bash $dir/Default.sh"
-	menuSeparator 
+	menuSeparator "| Simple |"
 	for style in "${styles[@]}"; do
 		menuItem "${count}. ${style%.*}" "bash $dir/$style"
 		count=$(($count+1))
+	done
+	menuSeparator "| Bitmap |"
+	for bmap in "${bitmap[@]}"; do
+		menuItem "${bcount}. ${bmap%-*} (Bitmap)" "bash $dir/$bmap"
+		bcount=$(($bcount+1))
 	done
 	menuEnd
 }
